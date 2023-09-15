c73345924a0b17b691868e80ddf3782d159cec0b
cbstange <>
2023-09-15 18:40:04 +0000

    Initial commit, add constants

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from random import randomint
+
+SHIP_LENGTH = [2,3,3,4,5]
+PLAYER_BRD = [[" "] * 8 for i in range(8)]
+COM_BRD = [[" "] * 8 for i in range(8)]
+PLAYER_GUESS_BRD = [[" "] * 8 for i in range(8)]
+COM_GUESS_BRD = [[" "] * 8 for i in range(8)]
+LET_TO_NUM = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, "G": 6, 'H': 7}
