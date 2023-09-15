459a69f5e042f3efa11804c294a2bee0b63cd982
cbstange <>
2023-09-15 18:42:03 +0000

    Add function to print basic board structure

--- run.py
+++ run.py
@@ -7,3 +7,15 @@ COM_BRD = [[" "] * 8 for i in range(8)]
 PLAYER_GUESS_BRD = [[" "] * 8 for i in range(8)]
 COM_GUESS_BRD = [[" "] * 8 for i in range(8)]
 LET_TO_NUM = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, "G": 6, 'H': 7}
+
+def display_board(board):
+    """
+    Display initial board play area starting at 1 
+    then iterating through row length.
+    """
+    print("  A B C D E F G H")
+    print(" -----------------")
+    row_num = 1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num += 1
